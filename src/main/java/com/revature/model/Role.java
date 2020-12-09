fae8237e1630b165626201905b25e6485111c36a
mareo yapp <mareo1997@gmail.com>
2020-12-09 09:56:16 -0500

    Added HQL to models

--- src/main/java/com/revature/model/Role.java
+++ src/main/java/com/revature/model/Role.java
@@ -1,9 +1,87 @@
 package com.revature.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Role")
 public class Role {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // this acts like the SERIAL datatype in SQL
+	@Column(name = "roleid")
 	private int roleid; //Trainer 1, Nurse 2, Admin 3
-	
+
+	@Column(name = "role", nullable = false)
 	private String role;
+
+	public Role() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Role(int roleid, String role) {
+		super();
+		this.roleid = roleid;
+		this.role = role;
+	}
+
+	public Role(String role) {
+		super();
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		String result = "RoleID: " + roleid + " \t\tRole: " + role + "\n";
+		return result;
+	}
+
+	public int getRoleid() {
+		return roleid;
+	}
+
+	public void setRoleid(int roleid) {
+		this.roleid = roleid;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + roleid;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (roleid != other.roleid)
+			return false;
+		return true;
+	}
 
 }
--- target/classes/Pokeapi.sql
+++ target/classes/Pokeapi.sql
@@ -1,12 +1,26 @@
-drop table pokemon cascade;
+drop table if exists pokemon cascade;
+drop table if exists statuscondition cascade;
+drop table if exists meds cascade;
+drop table if exists trainer cascade;
+drop table if exists patient cascade;
+drop table if exists role cascade;
+drop table if exists empl cascade;
+
+create table role(
+	roleid serial primary key,
+	ROLE varchar(100) NOT null
+);
 
 CREATE table trainer(
-	trainerid serial primary key,
-	firstname varchar(100) not null
+	trainerid integer primary key,
+	firstname varchar(100) not null,
+	hometown varchar(100) not null,
+	roleid integer not null,
+	FOREIGN KEY (roleid) REFERENCES role (roleid) on delete cascade
 );
 
 create table pokemon(
-	nationalid serial primary key,
+	nationalid integer primary key,
 	pokemonname varchar(100) not null unique,
 	type1 varchar(100) not null,
 	type2 varchar(100),
@@ -14,36 +28,65 @@ create table pokemon(
 	category varchar(100) not null
 );
 
+CREATE TABLE statuscondition(
+	statusid serial primary key,
+	status varchar(100) not null
+);
+
 create table meds(
 	medid serial primary key,
 	medname varchar(100) not null,
-	cost numeric not null
-	--, statusid numeric
+	cost numeric not null, 
+	statusid integer not null,
+	FOREIGN KEY (statusid) REFERENCES statuscondition (statusid) on delete cascade
 );
 
-create table role(
-	roleid serial primary key,
-	
+create table empl(
+	emplid serial primary key,
+	name varchar(100) not null,
+	roleid integer not null,
+	FOREIGN KEY (roleid) REFERENCES role (roleid) on delete cascade
 );
 
 create table patient(
 	pateintid serial primary key,
 	nationalid integer not null,
+	currenthp integer not null,
+	maxhp integer not null,
+	statusid integer not null,	
 	trainerid integer not null,
-	statusid integer not null,
-	nurseid integer not null,
+	nurseid integer, --assigned caretaker
+	medid integer, --meds applied
+--	nurseid integer not null,
 	FOREIGN KEY (nationalid) REFERENCES pokemon (nationalid) on delete cascade,
 	FOREIGN KEY (trainerid) REFERENCES trainer (trainerid) on delete cascade,
-	FOREIGN KEY (statusid) REFERENCES status (statusid) on delete cascade,
-	FOREIGN KEY (nurseid) REFERENCES nurse (nurseid) on delete cascade
+	FOREIGN KEY (statusid) REFERENCES statuscondition (statusid) on delete cascade,
+	FOREIGN KEY (nurseid) REFERENCES empl (emplid) on delete cascade,
+	FOREIGN KEY (medid) REFERENCES meds (medid) on delete cascade
 );
 
-create table nurse(
-	nurseid serial primary key,
-	firstname varchar(100) not null
-);
+INSERT into statuscondition (status) values ('Burn');
+INSERT into statuscondition (status) values ('Freeze');
 
-create table status(
-	statusid serial primary key,
-	status varchar(100)
-);
+INSERT into meds (medname, "cost", statusid) values ('Burn Heal', 250, 1);
+INSERT into meds (medname, "cost", statusid) values ('Ice Heal', 250, 2);
+
+insert into pokemon values (254, 'Sceptile', 'Grass', null, 'Overgrow', 'Forest');
+insert into pokemon values (392, 'Infernape', 'Fire', 'Fighting', 'Blaze', 'Flame');
+
+insert into "role" ("role") values ('Trainer');
+insert into "role" ("role") values ('Nurse');
+
+INSERT into trainer values (3, 'Brendan', 'Littleroot Town', 1);
+INSERT into trainer values (4, 'Lucas', 'Twinleaf Town', 1);
+
+INSERT into empl (name, roleid) values ('Joy', 2);
+
+insert into patient (nationalid, statusid, trainerid, currenthp, maxhp) values (254, 1, 3, 100, 254);
+insert into patient (nationalid, statusid, trainerid, currenthp, maxhp) values (392, 2, 4, 150, 200);
+
+select * from patient p ;
+
+update patient set statusid = 2 WHERE pateintid = 2;
+update patient set nurseid = 1 where pateintid = 1;
+update patient set medid = 1 where pateintid = 1;
