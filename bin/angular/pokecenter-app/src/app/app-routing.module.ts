4a2e3678b73f5752719d499aea28512b27ab8f68
Metsuryuu <qjroman@gmail.com>
2020-12-15 16:03:12 -0500

    bin update

--- bin/angular/pokecenter-app/src/app/app-routing.module.ts
+++ bin/angular/pokecenter-app/src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdmissionComponent } from './components/admission/admission.component';
+import { TableComponent } from './components/table/table.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { PasswordRecoveryComponent } from './components/password-recovery/password-recovery.component';
@@ -21,9 +24,60 @@ const routes: Routes = [
     component: SigninComponent
   },
   {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "admission",
+    component: AdmissionComponent
+  },
+  {
     path: "home",
     component: HomeComponent,
-    canActivate: [AuthGuard]
+  },
+  {
+    path: "table/view-my-pokemon",
+    component: TableComponent
+  },
+  {
+    path: "table/view-my-pokemon-stats",
+    component: TableComponent
+  },
+  {
+    path: "table/view-my-pokepatients",
+    component: TableComponent
+  },
+  {
+    path: "table/view-past-pokepatients",
+    component: TableComponent
+  },
+  {
+    path: "table/view-my-pokepatient-charts",
+    component: TableComponent
+  },
+  {
+    path: "table/get-poketreatment-by-patient-id",
+    component: TableComponent
+  },
+  {
+    path: "table/view-all-admitted-pokepatients",
+    component: TableComponent
+  },
+  {
+    path: "table/view-all-past-pokepatients",
+    component: TableComponent
+  },
+  {
+    path: "table/view-billing",
+    component: TableComponent
+  },
+  {
+    path: "table/view-current-med-stock",
+    component: TableComponent
+  },
+  {
+    path: "table/view-all-users",
+    component: TableComponent
   },
   {
     path: "",
