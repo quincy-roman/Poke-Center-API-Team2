d1897ffcb12027bffb610cefe239e49f801734ed
Metsuryuu <qjroman@gmail.com>
2020-12-16 14:10:06 -0500

    Small edit to admin

--- src/main/java/com/revature/controller/AdminController.java
+++ src/main/java/com/revature/controller/AdminController.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.Employee;
@@ -21,6 +23,8 @@ import com.revature.service.AdminService;
 import com.revature.util.ClientMessage;
 
 @RestController
+@RequestMapping(path = "/admin", consumes = "")
+@CrossOrigin(origins="http://localhost:4200")
 public class AdminController {
 	
 	@Autowired
