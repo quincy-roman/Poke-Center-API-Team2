d08a912fa34234887fa11cf3b843b6d2bb09051d
Metsuryuu <qjroman@gmail.com>
2020-12-17 14:35:15 -0500

    Cleaned up some imports

--- src/main/java/com/revature/controller/TrainerControllerImpl.java
+++ src/main/java/com/revature/controller/TrainerControllerImpl.java
@@ -1,9 +1,6 @@
 package com.revature.controller;
 
-import static com.revature.util.ClientMessageUtil.PATIENT_FAILED;
-import static com.revature.util.ClientMessageUtil.PATIENT_REGISTERED;
-import static com.revature.util.ClientMessageUtil.USER_NOT_REGISTERED;
-import static com.revature.util.ClientMessageUtil.USER_REGISTERED;
+import static com.revature.util.ClientMessageUtil.*;
 
 import java.util.List;
 
